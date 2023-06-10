7340165ec9a9f50974246b1b688d191904b761b2
Kirill So <kirill.sofronov@gmail.com>
2023-06-10 18:00:26 +0800

    Add files for presentation components: code, markdown and slide

---
+++ src/lib/deck/presentation.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import Slide from './slide.svelte';
+	import Code from './code.svelte';
+	import Markdown from './markdown.svelte';
+</script>
+
+<Slide animate>
+	<Code id="code" lines>
+		{`
+    function love() {}
+  `}
+	</Code>
+</Slide>
+
+<Slide animate>
+	<Code id="code" lines>
+		{`
+    function love() {
+      console.log('Love svelte')
+    }
+
+  `}
+	</Code>
+</Slide>
+
+<Slide animate>
+	<p class="uppercase">Transitions</p>
+
+	<div class="flex gap-2 place-content-center">
+		<div data-id="box1" class="w-[100px] h-[100px] bg-gray-400" />
+		<div data-id="box2" class="w-[100px] h-[100px] bg-gray-400" />
+		<div data-id="box3" class="w-[100px] h-[100px] bg-gray-400" />
+	</div>
+</Slide>
+<Slide animate>
+	<p class="uppercase">Transitions</p>
+
+	<div class="flex gap-2 place-content-center">
+		<div data-id="box1" class="w-[200px] h-[200px] bg-sky-400" />
+		<div data-id="box2" class="w-[200px] h-[200px] bg-green-400" />
+		<div data-id="box3" class="w-[200px] h-[200px] bg-teal-700" />
+	</div>
+</Slide>
+
+<Markdown>
+	{`
+		## Markdown
+		hello
+	`}
+</Markdown>
+
+<Markdown name="example.md" external />

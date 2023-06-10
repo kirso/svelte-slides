23fcbfe4f755db81e9d236fa6389064ded7d50f8
Kirill So <kirill.sofronov@gmail.com>
2023-06-10 15:21:06 +0800

    first commit

---
+++ src/lib/deck/slides.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import Reveal from 'reveal.js';
+	import Markdown from 'reveal.js/plugin/markdown/markdown';
+	import Highlight from 'reveal.js/plugin/highlight/highlight';
+	import Notes from 'reveal.js/plugin/notes/notes';
+	import 'reveal.js/dist/reveal.css';
+	import 'reveal.js/dist/theme/night.css';
+	import 'reveal.js/plugin/highlight/zenburn.css';
+
+	import Presentation from './presentation.svelte';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const deck = new Reveal({
+			autoAnimateEasing: 'ease',
+			autoAnimateDuration: 1,
+			hash: true,
+			controls: true,
+			progress: true
+		});
+
+		deck.initialize({
+			plugins: [Markdown, Highlight, Notes]
+		});
+	});
+</script>
+
+<div class="reveal">
+	<div class="slides">
+		<Presentation />
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Slides from '$lib/deck/slides.svelte';
+	import '../app.postcss';
+</script>
+
+<svelte:head>
+	<title>Svelte Slides | Presentation deck</title>
+</svelte:head>
+
+<Slides />
